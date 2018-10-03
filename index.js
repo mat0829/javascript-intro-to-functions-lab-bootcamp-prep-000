d5229e968d9ee834fe4d2f3c4b20b87d28390f2b
David Anderson <matandersonlv@hotmail.com>
2018-10-03 23:46:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function logWhisper(string) {
   return console.log("hello")
 }
 
+<<<<<<< HEAD
 function sayHiToGrandma (string) {
   if(string.toLowerCase() === string) {
     return "I can't hear you!"
@@ -25,3 +26,15 @@ function sayHiToGrandma (string) {
     else {return "I love you, too."
    }
     }
+=======
+function sayHiToGdrandma (string) {
+  if (string.toLowerCase() === 'hello') {
+    return "I can't hear you!"
+   }else if(string.toUpperCase() === 'HELLO'){
+      return "YES INDEED!" 
+    } else {
+        return "I love you, too"
+   }
+    
+}
+>>>>>>> de252336dfdf5ac376a3c295e40a98c532460ab7
